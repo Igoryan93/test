4c96f4807a892e1638d2f4cddd1b73fd1cb77ca1
Igor <lost.autumn93@gmail.com>
2020-12-17 18:35:20 +0600

    add flash message

--- public/index.php
+++ public/index.php
@@ -1,9 +1,27 @@
 <?php
+
+if( !session_id() ) @session_start();
+
+
 require_once "../vendor/autoload.php";
 
 use League\Plates\Engine;
 
+use \Tamtamchik\SimpleFlash\Flash;
+
+
 $templates = new Engine('../app/views/');
 
 echo $templates->render('about', ['name' => $user['username']]);
+
+
+
+
+flash()->info(['Info message', 'Second Info message']); /* Запись типа и текста сообщения */
+flash()->success(['Success message']);
+flash()->warning(['Warning message']);
+flash()->error(['Error message']);
+
+
+echo flash()->display(); /* Вывод всех записанных сообщений */
 
